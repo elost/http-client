7acfcbabbecd783f5f069160f27a1bd9d2bdce55
alex.obuhov <obukhov@cg.ru>
2016-12-22 00:50:33 +0300

    ZAGSSE-6488 Byte array http client for ZAGS report

--- src/main/java/net/elost/http_client/HttpResponse.java
+++ src/main/java/net/elost/http_client/HttpResponse.java
@@ -1,5 +1,7 @@
 package net.elost.http_client;
 
+import java.util.Arrays;
+
 public class HttpResponse {
   private final HttpMethod httpMethod;
   private final String url;
@@ -7,6 +9,7 @@ public class HttpResponse {
 
   private final int code;
   private final String responseBody;
+  private final byte[] responseBinaryBody;
 
   public HttpResponse(HttpMethod httpMethod, String url, String requestBody, int code, String responseBody) {
     this.httpMethod = httpMethod;
@@ -14,6 +17,16 @@ public class HttpResponse {
     this.requestBody = requestBody;
     this.code = code;
     this.responseBody = responseBody;
+    this.responseBinaryBody = null;
+  }
+
+  public HttpResponse(HttpMethod httpMethod, String url, String requestBody, int code, byte[] responseBinaryBody) {
+    this.httpMethod = httpMethod;
+    this.url = url;
+    this.requestBody = requestBody;
+    this.code = code;
+    this.responseBody = null;
+    this.responseBinaryBody = responseBinaryBody;
   }
 
   public HttpMethod getHttpMethod() {
@@ -36,12 +49,17 @@ public class HttpResponse {
     return responseBody;
   }
 
+  public byte[] getResponseBinaryBody() {
+    return responseBinaryBody;
+  }
+
   @Override
   public String toString() {
     return "request method: " + httpMethod.name() + " \n"
         + "request url: " + url + " \n"
         + "request body: " + requestBody + " \n"
         + "response code: " + code + " \n"
-        + "response body: " + responseBody;
+        + "response body: " + responseBody + " \n"
+        + "response binary body: " + Arrays.toString(responseBinaryBody);
   }
 }
